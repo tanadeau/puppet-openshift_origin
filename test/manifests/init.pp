24e0ca24e9b71c04f08bdf2df9acad51e312eb4f
Filirom1 <filirom1@gmail.com>
2013-04-22 11:10:58 +0200

    add centos support

--- test/manifests/init.pp
+++ test/manifests/init.pp
@@ -3,7 +3,7 @@ package { 'bind':
 }
 
 exec { 'generate tsig key':
-  command => "/usr/bin/rm -rf /var/named/Kexample.com* ; \
+  command => "/usr/bin/env rm -rf /var/named/Kexample.com* ; \
               /usr/sbin/dnssec-keygen -a HMAC-MD5 -b 512 -n USER -r /dev/urandom -K /var/named example.com",
   require => Package['bind'],
 }
