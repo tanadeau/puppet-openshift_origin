cdb22f68b51c39417daf9ca8fed57438b30cff07
N. Harrison Ripps <hripps@redhat.com>
2014-07-28 16:28:28 -0400

    Change file owner per feedback

--- manifests/mcollective_client.pp
+++ manifests/mcollective_client.pp
@@ -35,7 +35,7 @@ class openshift_origin::mcollective_client {
     ensure  => present,
     path    => "${::openshift_origin::params::ruby_scl_path_prefix}/etc/mcollective/client.cfg",
     content => template('openshift_origin/mcollective/mcollective-client.cfg.erb'),
-    owner   => 'root',
+    owner   => 'apache',
     group   => 'root',
     mode    => '0640',
     require => Package['mcollective-client'],
